90971f2e8cd0f62c898b1a80c3a4df978f8dbf1e
maria <marat1717@daum.net>
2023-05-29 21:22:28 +0900

    fix schema fix

--- api/grephql/schema.py
+++ api/grephql/schema.py
@@ -3,25 +3,28 @@ import graphene
 from graphene_django import DjangoObjectType
 from table.models.book import Book
 
-
-"""
-query{
-  allBook(number: "your_number_here") { # allBook
-    Name
-    number
-  }
-}
-"""
-
 class Books(DjangoObjectType):
     class Meta:
         model = Book
         fields = ("Name", "number")
 
 class Query(graphene.ObjectType):
-    all_Book = graphene.List(Books)
-    print(Book.objects.all())
-    def resolve_all_book(root, info):
-        return Book.objects.all()
+    # _a : A 이런식으로 언더바가 대문자를 뜻함
+    # all = graphene.List(Books)
+    # ===========================
+    # List : 리스트 타입 반환 , Fild : 단일 인스턴스만 반환 즉 하나만 반환
+    all = graphene.List(Books, args={
+        "number": graphene.String(required=False),
+    })
+
+    def resolve_all(root, info, number = None):
+        print(number)
+        print(Book.objects.filter(number=number))
+
+        # 리턴값은 다음과 같은 쿼리셋이 나와야한다 (<QuerySet [<Book: Book object (하나)>]>)
+        if number != None:
+            return Book.objects.filter(number=number)
+        else:
+            return Book.objects.all()
 
 schema = graphene.Schema(query=Query)
