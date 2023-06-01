391d2ea52a40ccfaafbc4c57d96a823e6586c596
maria <marat1717@daum.net>
2023-06-01 22:47:18 +0900

    Feat admin add

--- api/table/models/book.py
+++ api/table/models/book.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class Book(models.Model):
-    Name = models.CharField(primary_key=True, null=False, max_length=20)
+    name = models.CharField(primary_key=True, null=False, max_length=20)
     number = models.CharField(max_length=10, null=False)
