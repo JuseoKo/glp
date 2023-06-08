81050b37ad6740bbd60be86939e5e48e6a5fc429
maria <marat1717@daum.net>
2023-06-08 23:14:02 +0900

    Refactor refactoring

--- api/grephql/schema.py
+++ api/grephql/schema.py
@@ -3,7 +3,7 @@ import graphene
 from graphene_django import DjangoObjectType
 from table.models.test import Test
 
-class Tests(DjangoObjectType):
+class TestsObject(DjangoObjectType):
     class Meta:
         model = Test
 
@@ -12,16 +12,11 @@ class Query(graphene.ObjectType):
     # all = graphene.List(Books)
     # ===========================
     # List : 리스트 타입 반환 , Fild : 단일 인스턴스만 반환 즉 하나만 반환
-    all = graphene.List(Tests, args={
-        "number": graphene.String(required=False),
-    })
+    all = graphene.List(TestsObject)
 
-    def resolve_all(root, info, number = None):
+    def resolve_all(root, info):
 
         # 리턴값은 다음과 같은 쿼리셋이 나와야한다 (<QuerySet [<Book: Book object (하나)>]>)
-        if number != None:
-            return Book.objects.filter(number=number)
-        else:
-            return Book.objects.all()
+        return Test.objects.all()
 
 schema = graphene.Schema(query=Query)
